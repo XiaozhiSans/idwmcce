445f9f3b18eaa93c9a51b501ca7242233e78c22d
XiaozhiSans <xiaozhisans-awa@outlook.com>
2024-07-03 12:41:20 +0800

    🐞 fix: homepage cant scroll
    Ciallo～(∠・ω< )⌒☆

--- idwmcce.user.js
+++ idwmcce.user.js
@@ -2,7 +2,7 @@
 // @name			I Dont Want Minecraft China Edition
 // @name:zh			我不要网易我的世界
 // @namespace		idwmcce
-// @version			1.0
+// @version			1.1
 // @description		kill minecraft.net netease mc popup
 // @description:zh	自动隐藏mc官网网易我的世界弹窗
 // @license			MIT
@@ -14,7 +14,6 @@
 // @match			*://*.minecraft.net/*
 // @icon			https://fastly.jsdelivr.net/gh/XiaozhiSans/idwmcce@main/icon.png
 // @icon64			https://fastly.jsdelivr.net/gh/XiaozhiSans/idwmcce@main/icon.webp
-// @run-at			document-end
 // @grant			none
 // ==/UserScript==
 
@@ -29,7 +28,7 @@ try {
 	 * fix page can't scroll after hide the popup
 	 * 修复强制关闭弹窗后不让页面滚动
 	 */
-	document.body.classList.value = "page basicpage";
+	document.body.style.overflow = "auto";
 
 	console.log("[idwmcce] :) all done! have a nice day!");
 } catch {e => console.error(`[idwmcce] :( so sad, process failed\ncause:\n${e}`)}
